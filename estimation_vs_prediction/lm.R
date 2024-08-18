3bb04632cf95a1e0d4fafb40caa6de5e8197c2a0
Jack Hunt <jackmileshunt@gmail.com>
2024-08-18 19:51:03 +0100

    Update lm.R

--- estimation_vs_prediction/lm.R
+++ estimation_vs_prediction/lm.R
@@ -1,48 +1,34 @@
-library(ggplot2)
-
 set.seed(1234)
-x <- rnorm(100)
-y <- 2 * x + rnorm(100)
+
+data(mtcars)
+x <- mtcars$wt
+y <- mtcars$mpg
 
 model <- lm(y ~ x)
 model_summary <- summary(model)
-
-estimation_uncertainty <- model_summary$coefficients[, "Std. Error"]
-prediction_uncertainty <- predict(model, interval = "prediction", level = 0.95)
-
-est_err <- 2 * estimation_uncertainty
-pred_err_up <- prediction_uncertainty[, "upr"]
-pred_err_lwr <- prediction_uncertainty[, "lwr"]
+print(model_summary)
 
 data <- data.frame(x = x, y = y)
-plt <- ggplot(data, aes(x = x, y = y)) +
-  geom_point(color = "black", size = 3) +
-  geom_abline(
-    intercept = coef(model)[1],
-    slope = coef(model)[2],
-    color = "red", size = 1
-  ) +
-  geom_errorbar(
-    aes(
-      ymin = fitted(model) - est_err,
-      ymax = fitted(model) + est_err
-    ),
-    color = "blue", width = 0.1
-  ) +
-  geom_errorbar(
-    aes(
-      ymin = pred_err_lwr,
-      ymax = pred_err_up
-    ),
-    color = "green", width = 0.1
-  ) +
-  labs(
-    title = "Estimation vs Prediction Uncertainty",
-    x = "x",
-    y = "y"
-  ) +
-  theme_classic() +
-  theme(legend.position = "topright")
 
-ggsave("plot.pdf", plt, device = "pdf")
-print(plt)
+ci <- predict(model,
+  newdata = data, interval = "confidence",
+  level = 0.95
+)
+
+pi <- predict(model,
+  newdata = data, interval = "prediction",
+  level = 0.95
+)
+
+pdf("plot.pdf")
+
+plot(x, y, xlab = "Weight", ylab = "MPG", main = "MPG vs Weight")
+abline(model, col = "#ec0808")
+
+lines(x, ci[, 2], col = "blue", lty = 2)
+lines(x, ci[, 3], col = "blue", lty = 2)
+
+lines(x, pi[, 2], col = "orange", lty = 2)
+lines(x, pi[, 3], col = "orange", lty = 2)
+
+dev.off()
