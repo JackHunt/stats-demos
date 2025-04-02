9bef426f6324b489379107c5973293a91b974542
Jack Hunt <jackmileshunt@gmail.com>
2025-04-02 20:16:16 +0100

    Update classification.m

--- conformal/classification.m
+++ conformal/classification.m
@@ -14,14 +14,14 @@ X_cal = meas;
 Y_test = species;
 Y_test = categorical(Y_test);
 
-cv = cvpartition(Y_test, 'HoldOut', split);
+cv = cvpartition(Y_test, "HoldOut", split);
 X_train = X_cal(training(cv), :);
 Y_train = Y_test(training(cv));
 
 X_rem = X_cal(test(cv), :);
 Y_rem = Y_test(test(cv));
 
-cv2 = cvpartition(Y_rem, 'HoldOut', 0.5);
+cv2 = cvpartition(Y_rem, "HoldOut", 0.5);
 X_test = X_rem(test(cv2), :);
 Y_test = Y_rem(test(cv2));
 
@@ -35,7 +35,7 @@ Y_pred = predict(svm, X_train);
 
 figure;
 confusionchart(confusionmat(Y_train, Y_pred), categories(Y_train));
-title('Post Train Confusion Matrix');
+title("Post Train Confusion Matrix");
 
 %% Calibrate for Conformal Prediction
 [labels_cal, scores_cal] = predict(svm, X_cal);
@@ -47,7 +47,7 @@ q_hat = quantile(nonconformity_cal, q);
 
 figure;
 confusionchart(confusionmat(Y_cal, labels_cal), categories(Y_cal));
-title('Calibration Confusion Matrix');
+title("Calibration Confusion Matrix");
 
 %% Do Conformal Prediction
 [labels_test, scores_test] = predict(svm, X_test);
@@ -57,10 +57,10 @@ C = nonconformity_test <= q_hat;
 
 for n = 1 : size(X_test, 1)
     C_n = svm.ClassNames(find(C(n, :) > 0));
-    C_str = sprintf('{%s}', strjoin(cellstr(C_n), ', '));
-    fprintf('Y = %s, C = %s\n', Y_test(n), C_str);
+    C_str = sprintf("{%s}", strjoin(cellstr(C_n), ", "));
+    fprintf("Y = %s, C = %s\n", Y_test(n), C_str);
 end
 
 figure;
 confusionchart(confusionmat(Y_test, labels_test), categories(Y_test));
-title('Test Confusion Matrix');
+title("Test Confusion Matrix");
